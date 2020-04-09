5c1af624da7420cb7fdb09352c959bcb4057aad8
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2020-04-09 15:49:45 -0500

    Refine the script with the pandas - Python Data Analysis Library

--- testpostgres.py
+++ testpostgres.py
@@ -1,34 +1,56 @@
 import psycopg2
+import pandas
+import pandas.io.sql
 
 try:
+    print()
     print("Beginning Mr. Nuertey Odzeyem's stock_trading PostgreSQL database test...")
+    print()
+
     connect_str = "dbname='stock_trading' user='nuertey' host='localhost' " + \
                   "password='krobo2003'"
+
     # use our connection values to establish a connection
-    conn = psycopg2.connect(connect_str)
+    connection = psycopg2.connect(connect_str)
+
     # create a psycopg2 cursor that can execute queries
-    cursor = conn.cursor()
-    # create a new table with a single column called "name"
-    #cursor.execute("""CREATE TABLE tutorials (name char(40));""")
-    # run a SELECT statement - no data in there, but we can try it
+    cursor = connection.cursor()
     cursor.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
-    conn.commit() # <--- makes sure the change is shown in the database
+    connection.commit() # <--- makes sure the change is shown in the database
     rows = cursor.fetchall()
     print(rows)
-    cursor.execute("""SELECT * from trades""")
-    conn.commit() # <--- makes sure the change is shown in the database
-    rows = cursor.fetchall()
-    print(rows)
-    cursor.execute("""SELECT * from quotes""")
-    conn.commit() # <--- makes sure the change is shown in the database
-    rows = cursor.fetchall()
-    print(rows)
-    cursor.execute("""SELECT * from orders""")
-    conn.commit() # <--- makes sure the change is shown in the database
-    rows = cursor.fetchall()
-    print(rows)
-    cursor.close()
-    conn.close()
+    print()
+
+    cursor.execute("""SELECT * FROM information_schema.tables
+                    WHERE table_schema = 'public'""")
+    for table in cursor.fetchall():
+        print(table)
+
+    print()
+
+    trades = pandas.read_sql('select * from trades', connection)
+    quotes = pandas.io.sql.read_sql("SELECT * FROM quotes", connection)
+    orders = pandas.read_sql('select * from orders', connection)
+
+    print(trades)
+    print()
+
+    print(quotes)
+    print()
+
+    print(orders)
+    print()
+
 except Exception as e:
     print("Error! Can't connect to database. Invalid dbname, user or password...")
     print(e)
+
+finally:
+    #closing database connection.
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
+        print()
+
+    print("Ending Mr. Nuertey Odzeyem's stock_trading PostgreSQL database test...")
