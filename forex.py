7e0b1004ff0d1a3329455e6767fd7130bb19a5ff
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2020-04-11 22:18:24 -0500

    Changelog:
    [1] Cleanups and removed manual way of using pandas to query for FOREX and stock data. We do have GIT if we need to reference such manual code. Output of interpretation and execution is now as follows:
    
    		Time now:-> Sat Apr 11 22:16:02 2020
    
                                     USD/GHS               BTC/USD
    From_Currency Code                   USD                   BTC
    From_Currency Name  United States Dollar               Bitcoin
    To_Currency Code                     GHS                   USD
    To_Currency Name           Ghanaian Cedi  United States Dollar
    Exchange Rate                 5.72900000         6817.61000000
    Last Refreshed       2020-04-12 03:16:03   2020-04-12 03:16:02
    Time Zone                            UTC                   UTC
    Bid Price                     5.72900000         6817.37000000
    Ask Price                     5.73440000         6817.62000000
    
    HTTP Response Status Code:->  200
    HTTP Response Content Type:->  application/json
    HTTP Response Encoding:->  None
    
    1. Information:  Forex Daily Prices (open, high, low, close)
    2. From Symbol:  USD
    3. To Symbol:  GHS
    4. Output Size:  Full size
    5. Last Refreshed:  2020-04-10 21:35:00
    6. Time Zone:  UTC
    
    1. Information:  Intraday (1min) open, high, low, close prices and volume
    2. Symbol:  MSFT
    3. Last Refreshed:  2020-04-09 16:00:00
    4. Interval:  1min
    5. Output Size:  Full size

--- forex.py
+++ forex.py
@@ -26,17 +26,15 @@ import matplotlib.pyplot as plt
 # they granted me for Software Development purposes:
 API_KEY = 'MZ9Q5U40PRWBMA6T'
 os.environ["ALPHAVANTAGE_API_KEY"] = "MZ9Q5U40PRWBMA6T"
-#print(os.environ["ALPHAVANTAGE_API_KEY"])
 
 print('\t\tTime now:-> %s\n' % time.ctime())
 
 # Retrieve realtime United States Dollar to Ghana Cedi (USD/GHS),
 # Cameroon Central African CFA Franc BEAC, Chinese Yuan, Japanese Yen, etc. 
 # exchange rates and multiple other Bitcoin exchange rate pairs:
-#forex = pdr.DataReader(["USD/XAF", "USD/GHS", "USD/CNY",
-#                        "BTC/CNY", "BTC/USD"], "av-forex", 
-#                        api_key=os.getenv('ALPHAVANTAGE_API_KEY'))
-#print(forex)
+forex = pdr.DataReader(["USD/GHS", "BTC/USD"], "av-forex", 
+                        api_key=os.getenv('ALPHAVANTAGE_API_KEY'))
+print(forex)
 print()
 
 ########################################################################
@@ -45,7 +43,6 @@ print()
 ########################################################################
 
 # Behold, the power of Requests:
-#r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
 r = requests.get('https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=GHS&outputsize=compact&apikey=MZ9Q5U40PRWBMA6T')
 
 if r.ok:
@@ -55,51 +52,8 @@ if r.ok:
     print("HTTP Response Encoding:-> ", r.encoding)
     print()
     
-    # This way fails interpretation with the following error:
-    #
-    # pandas.errors.ParserError: Error tokenizing data. C error: Expected 1 fields in line 3, saw 5
-    #timeSeriesFX = pandas.read_csv(io.StringIO(r.content.decode('utf-8')))
-    
-    # This way fails interpretation with the following error:
-    #
-    # pandas.errors.ParserError: Error tokenizing data. C error: Expected 1 fields in line 3, saw 5
-    #timeSeriesFX = pandas.read_csv(io.StringIO(r.text))
-    
-    # This way fails interpretation with the following error:
-    #
-    # TypeError: initial_value must be str or None, not Response
-    #timeSeriesFX = pandas.read_csv(io.StringIO(r))
-    
-    # This way seems to work, though with some output NaN values and in strictly JSON format:
-    #timeSeriesFX = pandas.read_json(r.text)
-    
-    # This way works; also in strictly JSON format:
-    #timeSeriesFX = pandas.DataFrame.from_dict(r.json())
-    
-    # This way fails interpretation with the following error:
-    #
-    # ValueError: Invalid file path or buffer object type: <class 'dict'>
-    #timeSeriesFX = pandas.read_json(r.json())
-    #timeSeriesFX = timeSeriesFX.set_index("date")
-    
-    #print("1. Information: ", timeSeriesFX['Meta Data']['1. Information'])
-    #print("2. From Symbol: ", timeSeriesFX['Meta Data']['2. From Symbol'])
-    #print("3. To Symbol: ", timeSeriesFX['Meta Data']['3. To Symbol'])
-    #print("4. Last Refreshed: ", timeSeriesFX['Meta Data']['4. Last Refreshed'])
-    #print("5. Time Zone: ", timeSeriesFX['Meta Data']['5. Time Zone'])
-    #print()
-    
-    #timeSeriesFX.head() # This will give us the first 5 lines of data
-    #timeSeriesFX.tail() #This will give us the last 5 lines of data
-    
-    #print(timeSeriesFX['Time Series FX (Weekly)'])
-    #print()
-    
     fe = ForeignExchange(key=API_KEY, output_format='pandas')
     data, meta_data = fe.get_currency_exchange_daily(from_symbol='USD', to_symbol='GHS', outputsize='full')
-    
-    #print(meta_data)
-    #print()
 
     print("1. Information: ", meta_data['1. Information'])
     print("2. From Symbol: ", meta_data['2. From Symbol'])
@@ -109,14 +63,6 @@ if r.ok:
     print("6. Time Zone: ", meta_data['6. Time Zone'])
     print()
     
-    #print(data)
-    #print()
-    #data['4. close'].plot()
-    #data['4. close'].plot()
-    #plt.title('Forex Daily Prices - US Dollars to Ghanaian Cedis')
-    #plt.show()
-
-    #plt.figure(figsize=(12,5))
     plt.title('Forex Daily Prices - US Dollars to Ghanaian Cedis')
     ax1 = data['3. low'].plot(color='blue', grid=True, label='low')
     ax2 = data['2. high'].plot(color='red', grid=True, secondary_y=True, label='high')
@@ -136,14 +82,13 @@ if r.ok:
     print("5. Output Size: ", meta_data['5. Output Size'])
     print()
     
-    #print(data)
-    #print()
-    
-    data['4. close'].plot()
     plt.title('Intraday Times Series for the MSFT stock (1 min)')
+    ax1 = data['3. low'].plot(color='blue', grid=True, label='low')
+    ax2 = data['2. high'].plot(color='red', grid=True, secondary_y=True, label='high')
+    ax3 = data['4. close'].plot(color='green', grid=True, secondary_y=True, label='close')
+    ax1.legend(loc=1)
+    ax2.legend(loc=2)
+    ax3.legend(loc=3)
     plt.show()
-    
-    # How to reset the index of a DataFrame
-    #Dataset.dropna(inplace=True)
 else:
     print('Error! Issue with the URL, HTTP Request, and/or the HTTP Response')
