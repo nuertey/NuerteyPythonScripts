b8191f821e4ae1246c8cf5eff566a1b3839ea843
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2020-04-11 19:38:23 -0500

    Added another test scenario to show the proper way of using the alpha_vantage python module

--- forex.py
+++ forex.py
@@ -16,6 +16,8 @@ import time
 import requests
 import pandas
 import pandas_datareader as pdr
+from alpha_vantage.timeseries import TimeSeries
+import matplotlib.pyplot as plt
 
 # Forex: Alpha Vantage provides realtime currency exchange rates 
 # (for physical and digital currencies). Below is the access key that 
@@ -41,15 +43,22 @@ print()
 
 # Behold, the power of Requests:
 #r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
-r = requests.get('https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=GHS&outputsize=full&apikey=MZ9Q5U40PRWBMA6T')
+r = requests.get('https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=GHS&outputsize=compact&apikey=MZ9Q5U40PRWBMA6T')
 
 # The response fields...:
-print(r.status_code)
-print(r.headers['content-type'])
-print(r.encoding)
+print("HTTP Response Status Code:-> ", r.status_code)
+print("HTTP Response Content Type:-> ", r.headers['content-type'])
+print("HTTP Response Encoding:-> ", r.encoding)
 #print(r.text)
 #print(r.json())
 print()
+
+#if r.ok:
+#    data = r.content.decode('utf8')
+#    df = pandas.read_csv(io.StringIO(data))
+#    print(df)
+#else:
+#    print('Error! Issue with the URL, HTTP Request, and/or the HTTP Response')
 
 # This way fails interpretation with the following error:
 #
@@ -76,4 +85,34 @@ timeSeriesFX = pandas.DataFrame.from_dict(r.json())
 #
 # ValueError: Invalid file path or buffer object type: <class 'dict'>
 #timeSeriesFX = pandas.read_json(r.json())
-print(timeSeriesFX)
+#timeSeriesFX = timeSeriesFX.set_index("date")
+
+print("1. Information: ", timeSeriesFX['Meta Data']['1. Information'])
+print("2. From Symbol: ", timeSeriesFX['Meta Data']['2. From Symbol'])
+print("3. To Symbol: ", timeSeriesFX['Meta Data']['3. To Symbol'])
+print("4. Last Refreshed: ", timeSeriesFX['Meta Data']['4. Last Refreshed'])
+print("5. Time Zone: ", timeSeriesFX['Meta Data']['5. Time Zone'])
+print()
+
+#timeSeriesFX.head() # This will give us the first 5 lines of data
+#timeSeriesFX.tail() #This will give us the last 5 lines of data
+
+print(timeSeriesFX['Time Series FX (Weekly)'])
+print()
+
+ts = TimeSeries(key='MZ9Q5U40PRWBMA6T', output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='MSFT', interval='1min', outputsize='full')
+
+print("1. Information: ", meta_data['1. Information'])
+print("2. Symbol: ", meta_data['2. Symbol'])
+print("3. Last Refreshed: ", meta_data['3. Last Refreshed'])
+print("4. Interval: ", meta_data['4. Interval'])
+print("5. Output Size: ", meta_data['5. Output Size'])
+print()
+
+print(data)
+print()
+
+data['4. close'].plot()
+plt.title('Intraday Times Series for the MSFT stock (1 min)')
+plt.show()
