43d999ea4554b83ce6b435e01eba2ba6e5d779ce
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2020-05-16 00:54:59 -0500

    Yet another checkpoint before trying to get at the json column dictionary

--- nuertey_news_item.py
+++ nuertey_news_item.py
@@ -16,6 +16,9 @@ import os
 import sys
 import argparse
 import datetime
+import re
+import requests
+import json
 import pandas as pd
 
 pd.set_option('display.max_rows', 100)
@@ -23,7 +26,7 @@ pd.set_option('display.max_rows', 100)
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         usage="%(prog)s [-h] | [-v] | [-c country] | [-t topic] [-b begin_date]",
-        description="Nuertey's News Aggregator. What is a country talking about most today? Or, what is the world talking about concerning a particular topic beginning from a particular date till now?",
+        description="Welcome, Velkommen, Woé zɔ, Karibu, Mo-heee to Nuertey's News Aggregator. What is a country talking about most today? Or, what is the world talking about concerning a particular topic beginning from a particular date till now? What do you want to know? Ask me in the format below:",
         allow_abbrev=False,
         add_help=True
     )
@@ -39,12 +42,12 @@ def init_argparse() -> argparse.ArgumentParser:
     parser.add_argument(
         "-t", "--topic", action='store',
         type=str,
-        help="Specify the topic you are interested in."
+        help="Specify the topic you are interested in world-wide."
     )
     parser.add_argument(
         "-b", "--begin_date", action='store', 
         type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'),
-        help="Specify the beginning date from which to query for all news articles. Format: yyyy-mm-dd"
+        help="Specify the beginning date from which to query for all news articles. Date format is: yyyy-mm-dd"
     )
     return parser
 
@@ -54,14 +57,11 @@ country = args.country
 topic = args.topic
 begin_date = args.begin_date
 
-print(country)
-print()
+#print(country)
+#print()
 
-print(topic)
-print()
-
-print(begin_date)
-print()
+#print(topic)
+#print()
 
 if country is None and topic is None:
     print("No primary arguments specified. Exiting...")
@@ -69,10 +69,36 @@ if country is None and topic is None:
 elif country is not None:
     print("Querying for {0} top news headlines...".format(country.capitalize()))
     token = open(".newsapi_token").read().rstrip('\n')
-    print(token)
-    print()
+    news_api_url = f"https://newsapi.org/v2/top-headlines?country={country}&apiKey={token}"
+    reply = requests.get(news_api_url)
+    if reply.ok:
+        #print("HTTP Response Status Code:-> ", reply.status_code)
+        #print("HTTP Response Content Type:-> ", reply.headers['content-type'])
+        #print("HTTP Response Encoding:-> ", reply.encoding)
+        #print()
+
+        text_data = reply.text
+        #print(text_data)
+        #print()
+        json_dict = json.loads(text_data)
+        #print(json_dict)
+        #print()
+        data = pd.DataFrame.from_dict(json_dict["articles"])
+        sources = data['source']
+        d = { k: [sources[:1][k]] for k in ['name'] }
+        df = pd.DataFrame(d)
+        print(df)
+        print()
+        
+        print(data[['source', 'publishedAt', 'content']])
+        print()
+    else:
+        print('Error! Issue with the URL, HTTP Request, and/or the HTTP Response')
+        sys.exit()
 elif topic is not None and begin_date is not None:
-    print("Querying all world-wide news headlines for topics on \"{0}\" from {1} till now...".format(topic, begin_date))
+    #print(begin_date.strftime('%Y-%m-%d'))
+    print()
+    print("Querying all world-wide news headlines for topics on \"{0}\" from {1} till now...".format(topic, begin_date.strftime('%Y-%m-%d')))
     token = open(".newsapi_token").read().rstrip('\n')
     print(token)
     print()
