5bca8569555dedfc4f076a6265e40fd51e63d0b4
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2020-06-13 16:48:18 -0500

    Lots of trial and errors...

--- nuertey_trending_topics.py
+++ nuertey_trending_topics.py
@@ -11,7 +11,7 @@
 #  Created: June 13, 2020
 #   Author: Nuertey Odzeyem
 #**********************************************************************/
-#import pprint
+import json
 import time
 import datetime
 import pytrends
@@ -23,25 +23,27 @@ import matplotlib.pyplot as plt
 from datetime import datetime, date, time
 from pytrends.request import TrendReq
 
-#def myprint(d, ident=0):
-#    for k, v in d.items():
-#        if isinstance(v, dict):
-#            myprint(v, ident+1)
-#        else:
-#            print('\t' * (ident+1) + "{0} : {1}".format(k, v))
-#
-#def pretty(d, indent=0):
-#    for key, value in d.items():
-#        print('\t' * indent + str(key))
-#        if isinstance(value, dict):
-#            pretty(value, indent+1)
-#        else:
-#            #print('\t' * (indent+1) + str(value))
-#            print()
-#            category_data = pd.DataFrame.from_dict(category_data)
-#            category_data = category_data['children'].apply(pd.Series)
-#            print(category_data)
-#            print()
+def myprint(d, ident=0):
+    for k, v in d.items():
+        if isinstance(v, dict):
+            myprint(v, ident+1)
+        elif k != 'children':
+            print('\t' * (ident+1) + "{0} : {1}\n".format(k, v))
+
+def pretty(d, indent=0):
+    next_key = None
+    temp = iter(d) 
+    for iter_key, (key, value) in zip(temp, d.items()):
+        # print('\t' * indent + str(key))
+        next_key = next(temp, None)
+        if iter_key == 'children' and next_key != 'children' and next_key != None:
+            category_data = pd.DataFrame.from_dict(value)
+            #category_data = category_data['children'].apply(pd.Series)
+            print(category_data)
+            print()
+        else:
+            #print('\t' * (indent+1) + str(value))
+            pretty(value, indent+1)
 
 # Run output of script for Emile on topics trending in Cameroon by region
 # ISO-3166-2 and on the category, "politics". Plot it.
@@ -66,6 +68,15 @@ pytrend = TrendReq()
 
 # Returns Categories dictionary for potential usage in help text.
 all_categories = pytrend.categories()
+#print(all_categories.items())
+#print()
+#print(all_categories.keys())
+#print()
+#print(all_categories.values())
+#print()
+# keyList = sorted(all_categories.keys())
+# print(keyList)
+# print()
 #all_categories = pd.DataFrame(all_categories)
 #all_categories = pd.concat({k: pd.DataFrame(v).T for k, v in all_categories.items()}, axis=0)
 all_categories_data = pd.DataFrame.from_dict(all_categories)
@@ -75,9 +86,15 @@ main_categories_data = all_categories_data[['name', 'id']]
 print(main_categories_data)
 print()
 
+#json_dict = json.loads(str(all_categories))
+#data_list = [ {'name': country, 'id': identifier} for country, identifier in zip(all_categories['name'], all_categories['id']) ] 
+# data = pd.DataFrame(all_categories, columns=['name', 'id'])
+# print(data)
+# print()
 #pp = pprint.PrettyPrinter(width=160)
 #pp.pprint(dict(all_categories))
 #print()
+myprint(all_categories)
 
 # TBD Nuertey Odzeyem, ask Wayo when he wakes, "Would 'topic keyword', 'country' 
 # and 'category' inputs to this new python script satisfy his requirement 
@@ -91,7 +108,8 @@ print()
 # python nuertey_trending_topics.py -c ghana -g fashion -t kente "
 #
 # where -c denotes country for which to monitor for realtime trending topics (list of ISO_3166-2 country codes will be displayed in help text)
-#       -g denotes Google Trends Categories listing (list of 1427 category entries will be displayed in help text)
+#       -g denotes Google Trends Categories listing (list of 25 major category entries will be displayed in help text)
+#       -v denotes Google Trends Categories listing (verbosely list all 1427 categories and sub-categories recursively in help text)
 #       -t denotes a trending topic keyword of interest
 
 # Create payload and capture API tokens. Only needed for interest_over_time(),
