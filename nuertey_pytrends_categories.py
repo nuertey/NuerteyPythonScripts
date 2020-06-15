d0edb0781fa77194f8679a0dce54a9811ccbd768
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2020-06-14 21:20:45 -0500

    Checkpointing massive brainstorming changes.

--- nuertey_pytrends_categories.py
+++ nuertey_pytrends_categories.py
@@ -1,28 +1,15 @@
-import os
-import sys
-import argparse
-import time
-import datetime
+import ast
+import urllib
 import pytrends
-import pycountry
 import collections
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from datetime import datetime, date, time
 from pytrends.request import TrendReq
-from argparse import RawTextHelpFormatter
-from itertools import chain
 
 pd.set_option('display.max_rows', 100)
 
-def myprint(d, ident=0):
-    for k, v in d.items():
-        if isinstance(v, dict):
-            myprint(v, ident+1)
-        elif k != 'children':
-            print('\t' * (ident+1) + "{0} : {1}\n".format(k, v))
+category_names_list = []
+category_ids_list   = []
 
 # The yield from operator returns an item from a generator one at a time.
 # This syntax for delegating to a subgenerator was added in 3.3
@@ -33,37 +20,53 @@ def flatten(l):
         else:
             yield el
 
-for ele in d.values():
-    if isinstance(ele,dict):
-       for k, v in ele.items():
-           print(k,' ',v)
-
 def RecursiveTraverse(nested_categories, indent=0):
-    for element in nested_categories.values():
-        element = str(element).strip('[').strip(']')
-        if isinstance(element, dict):
-            for key, value in element.items():
-                # print('\t' * indent + str(key))
-                if key == 'children':
-                    RecursiveTraverse(value, indent+1)
-                elif key == 'name':
-                    #print('\t' * (indent+1) + str(value))
-                    category_data = pd.DataFrame.from_dict(value)
-                    #category_data = category_data['children'].apply(pd.Series)
-                    print(category_data)
-                    print()
-
-        else:
-            print('\t' * (indent+1) + "Warning! Not a dictionary:")
-            print('\t' * (indent+1) + str(element))
-            print()
+    if isinstance(nested_categories, collections.abc.Mapping):
+        print('\t' * (indent+1) + "Success! Expected! A dictionary.")
+        print()
+        culprit_line = ""
+        for key, value in nested_categories.items():
+            #print(key)
+            #print()
+            #print(value)
+            #print()
+            if key == 'children':
+                #nested_categories[key] = str(value).strip('[').strip(']')
+                #value = str(value).strip('[').strip(']')
+                value = str(value)[1:-1]
+                value = urllib.quote("'{}'".format(value))
+                print(value)
+                print()
+                value = ast.literal_eval(value) 
+                RecursiveTraverse(value, indent+1)
+            elif key == 'name':
+                culprit_line = '\t' * (indent+1) + str(value)
+                category_names_list.append(value)
+            elif key == 'id':
+                culprit_line = culprit_line + " : " + str(value)
+                category_ids_list.append(value)
+                print(culprit_line)
+                print()
+    else:
+        print('\t' * (indent+1) + "Warning! Unexpected! Not a dictionary:")
+        print('\t' * (indent+1) + str(nested_categories))
+        print()
 
 pytrend = TrendReq()
 all_categories = pytrend.categories()
-print(all_categories)
-print()
+#print(all_categories)
+#print()
+
 all_categories_data = pd.DataFrame.from_dict(all_categories)
 all_categories_data = all_categories_data['children'].apply(pd.Series)
+#print(all_categories_data)
+#print()
+
+main_categories_data = all_categories_data[['name', 'id']]
+#print(main_categories_data)
+#print()
+
+RecursiveTraverse(all_categories)
+all_categories_data = pd.DataFrame({'name': category_names_list, 'id': category_ids_list})
 print(all_categories_data)
 print()
-main_categories_data = all_categories_data[['name', 'id']]
