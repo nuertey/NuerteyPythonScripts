87813537e3e88ac5a3aaee928e8c6bf306d61dc5
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2020-06-28 17:08:33 -0500

    Changes due to following tutorial github code more closely.

--- nuertey_flask_project_1/app.py
+++ nuertey_flask_project_1/app.py
@@ -1,9 +1,14 @@
 import os
 import sys
 import json
+from datetime import datetime
 from flask import Flask
 from flask import jsonify
+from flask import request, Response
 from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPTokenAuth
+from functools import wraps
+from passlib.hash import pbkdf2_sha256 as hasher
 
 # An app object is an instance of the Flask object. It'll act as the central 
 # configuration object for the entire application. It's used to set up pieces 
@@ -33,10 +38,10 @@ app = Flask(__name__)
 # configuration a map from the specified route to the function that runs 
 # when that route is accessed. You can use this to start building out the 
 # rest of the API.
-@app.route('/')
-def hello_world():
-    """Print 'Hello, world!' as the response body."""
-    return 'Hello, world!'
+#@app.route('/')
+#def hello_world():
+#    """Print 'Hello, world!' as the response body."""
+#    return 'Hello, world!'
 
 # This is the most basic complete Flask application. app is an instance of 
 # Flask, taking in the __name__ of the script file. This lets Python know 
@@ -60,7 +65,14 @@ def hello_world():
 # setup.py. You don't have to do anything else with it; flask-sqlalchemy 
 # will recognize Postgres from the database URL.
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+auth = HTTPTokenAuth(scheme='Token')
 db = SQLAlchemy(app)
+
+# Beware of circular dependency by ensuring that the db is created first
+# before importing the models.
+from .models import Task, User
+
+INCOMING_DATE_FMT = '%d/%m/%Y %H:%M:%S'
 
 # Start with a view that handles only GET requests, and respond with the 
 # JSON representing all the routes that will be accessible and the methods 
@@ -75,12 +87,162 @@ db = SQLAlchemy(app)
 # and the client asked for the route without the slash, you wouldn't need 
 # to double up on decorators. Flask would redirect the client's request
 # appropriately. It's odd that it doesn't work both ways.
-@app.route('/api/v1', methods=["GET"])
-@app.route('/api/v1/', methods=["GET"])
-def info_view():
+#@app.route('/api/v1', methods=["GET"])
+#@app.route('/api/v1/', methods=["GET"])
+#def info_view():
+#    """List of routes for this API."""
+#    # A good structure that contains complexity but can still be stringified is a Python dictionary. Therefore, I recommend that, whenever you want to send some data to the client, you choose a Python dict with whatever key-value pairs you need to convey information. To turn that dictionary into a properly formatted JSON response, headers and all, pass it as an argument to Flask's jsonify function (from flask import jsonify).
+#    output_dictionary = {
+#        'info': 'GET /api/v1',
+#        'register': 'POST /api/v1/accounts',
+#        'single profile detail': 'GET /api/v1/accounts/<username>',
+#        'edit profile': 'PUT /api/v1/accounts/<username>',
+#        'delete profile': 'DELETE /api/v1/accounts/<username>',
+#        'login': 'POST /api/v1/accounts/login',
+#        'logout': 'GET /api/v1/accounts/logout',
+#        "user's tasks": 'GET /api/v1/accounts/<username>/tasks',
+#        "create task": 'POST /api/v1/accounts/<username>/tasks',
+#        "task detail": 'GET /api/v1/accounts/<username>/tasks/<id>',
+#        "task update": 'PUT /api/v1/accounts/<username>/tasks/<id>',
+#        "delete task": 'DELETE /api/v1/accounts/<username>/tasks/<id>'
+#    }
+#    return jsonify(output_dictionary)
+
+# The next thing is that Flask's route patterns can have a bit more nuance. 
+# One scenario is a hardcoded route that must be matched perfectly to 
+# activate a view function. Another scenario is a route pattern that can 
+# handle a range of routes, all mapping to one view by allowing a part 
+# of that route to be variable. If the route in question has a variable, 
+# the corresponding value can be accessed from the same-named variable 
+# in the view's parameter list:
+#
+# @app.route('/a/sample/<variable>/route')
+# def some_view(variable):
+#     # some code blah blah blah
+
+#@app.route('/api/v1/accounts/<username>/tasks', methods=['POST'])
+#def create_task(username):
+#    """Create a task for one user."""
+#    # With Flask, you can ask for the User object directly through the query 
+#    # attribute for the instance matching your criteria. This type of 
+#    # query would provide a list of objects (even if it's only one object 
+#    # or none at all), so to get the object you want, just call first().
+#    user = User.query.filter_by(username=username).first()
+#    if user:
+#        # Whenever data is sent to the application, regardless of the 
+#        # HTTP method used, that data is stored on the form attribute 
+#        # of the request object. The name of the field on the frontend 
+#        # will be the name of the key mapped to that data in the form 
+#        # dictionary. It'll always come in the form of a string, so if 
+#        # you want your data to be a specific data type, you'll have to 
+#        # make it explicit by casting it as the appropriate type.
+#        task = Task(
+#            name=request.form['name'],
+#            note=request.form['note'],
+#            creation_date=datetime.now(),
+#            due_date=datetime.strptime(due_date, INCOMING_DATE_FMT) if due_date else None,
+#            completed=bool(request.form['completed']),
+#
+#            # The other thing to note is the assignment of the current 
+#            # user's user ID to the newly instantiated Task. This is how 
+#            # that foreign key relationship is maintained.
+#            user_id=user.id,
+#        )
+#
+#        # The db.session.add(task) stages the new Task instance to be 
+#        # added to the table, but doesn't add it yet. While it's done 
+#        # only once here, you can add as many things as you want before 
+#        # committing. The db.session.commit() takes all the staged 
+#        # changes, or "commits," and applies them to the corresponding 
+#        # tables in the database.
+#        db.session.add(task)
+#        db.session.commit()
+#
+#        # The response is an actual instance of a Response object with 
+#        # its mimetype, body, and status set deliberately. The goal for 
+#        # this view is to alert the user they created something new. 
+#        # Seeing how this view is supposed to be part of a backend API 
+#        # that sends and receives JSON, the response body must be JSON 
+#        # serializable. A dictionary with a simple string message should 
+#        # suffice. Ensure that it's ready for transmission by calling 
+#        # json.dumps on your dictionary, which will turn your Python 
+#        # object into valid JSON. This is used instead of jsonify, as 
+#        # jsonify constructs an actual response object using its input 
+#        # as the response body. In contrast, json.dumps just takes a 
+#        # given Python object and converts it into a valid JSON string 
+#        # if possible.
+#        output = {'msg': 'posted'}
+#        response = Response(
+#            mimetype="application/json",
+#            response=json.dumps(output),
+#
+#            # By default, the status code of any response sent with Flask 
+#            # will be 200. That will work for most circumstances, where 
+#            # you're not trying to send back a specific redirection-level 
+#            # or error-level message. Since this case explicitly lets the 
+#            # frontend know when a new item has been created, set the 
+#            # status code to be 201, which corresponds to creating a new 
+#            # thing.
+#            status=201
+#        )
+#        return response
+
+# 200 OK
+#     The request has succeeded. The meaning of the success depends on the HTTP method:
+# 
+#         GET: The resource has been fetched and is transmitted in the message body.
+#         HEAD: The entity headers are in the message body.
+#         PUT or POST: The resource describing the result of the action is transmitted in the message body.
+#         TRACE: The message body contains the request message as received by the server
+# 
+# 201 Created
+#     The request has succeeded and a new resource has been created as a result. This is typically the response sent after POST requests, or some PUT requests.
+# 
+
+def forbidden_response():
+    """Return an HTTP response when the user is forbidden."""
+    response = Response(
+        mimetype="application/json",
+        response=json.dumps({'error': 'You do not have permission to access this profile.'}),
+        status=403
+    )
+    return response
+
+
+def notfound_response():
+    """Return an HTTP response when a nonexistant profile has been searched for."""
+    response = Response(
+        mimetype="application/json",
+        response=json.dumps({'error': 'The profile does not exist'}),
+        status=404
+    )
+    return response
+
+def get_profile(username):
+    """Check if the requested profile exists."""
+    return Profile.query.filter_by(username=username).first()
+
+
+@auth.verify_token
+def verify_token(token):
+    """Verify that the incoming request has the expected token."""
+    if token:
+        username = token.split(':')[0]
+        profile = get_profile(username)
+        return token == profile.token
+
+
+def authenticate(response, profile):
+    """Authenticate an outgoing response with the user's token."""
+    token = f'{profile.username}:{profile.token}'
+    response.set_cookie('auth_token', value=token)
+    return response
+
+
+@app.route('/api/v1')
+def index():
     """List of routes for this API."""
-    # A good structure that contains complexity but can still be stringified is a Python dictionary. Therefore, I recommend that, whenever you want to send some data to the client, you choose a Python dict with whatever key-value pairs you need to convey information. To turn that dictionary into a properly formatted JSON response, headers and all, pass it as an argument to Flask's jsonify function (from flask import jsonify).
-    output_dictionary = {
+    output = {
         'info': 'GET /api/v1',
         'register': 'POST /api/v1/accounts',
         'single profile detail': 'GET /api/v1/accounts/<username>',
@@ -94,16 +256,187 @@ def info_view():
         "task update": 'PUT /api/v1/accounts/<username>/tasks/<id>',
         "delete task": 'DELETE /api/v1/accounts/<username>/tasks/<id>'
     }
-    return jsonify(output_dictionary)
+    response = jsonify(output)
+    return response
 
-# The next thing is that Flask's route patterns can have a bit more nuance. 
-# One scenario is a hardcoded route that must be matched perfectly to 
-# activate a view function. Another scenario is a route pattern that can 
-# handle a range of routes, all mapping to one view by allowing a part 
-# of that route to be variable. If the route in question has a variable, 
-# the corresponding value can be accessed from the same-named variable 
-# in the view's parameter list:
-#
-# @app.route('/a/sample/<variable>/route')
-# def some_view(variable):
-#     # some code blah blah blah
+
+@app.route('/api/v1/accounts', methods=['POST'])
+def register():
+    """Add a new user profile if it doesn't already exist."""
+    needed = ['username', 'email', 'password', 'password2']
+    if all([key in request.form for key in needed]):
+        username = request.form['username']
+        profile = get_profile(username)
+        if not profile:
+            if request.form['password'] == request.form['password2']:
+                new_profile = Profile(
+                    username=username,
+                    email=request.form['email'],
+                    password=hasher.hash(request.form['password']),
+                )
+                db.session.add(new_profile)
+                db.session.commit()
+
+                response = Response(
+                    response=json.dumps({"msg": 'Profile created'}),
+                    status=201,
+                    mimetype="application/json"
+                )
+                return authenticate(response, new_profile)
+
+            response = jsonify({"error": "Passwords don't match"})
+            response.status_code = 400
+            return response
+
+        response = jsonify({'error': f'Username "{username}" is already taken'})
+        response.status_code = 400
+        return response
+
+    response = jsonify({'error': 'Some fields are missing'})
+    response.status_code = 400
+    return response
+
+
+@app.route('/api/v1/accounts/login', methods=['POST'])
+def login():
+    """Authenticate a user."""
+    needed = ['username', 'password']
+    if all([key in request.forms for key in needed]):
+        profile = get_profile(request.forms['username'])
+        if profile and hasher.verify(request.forms['password'], profile.password):
+            response = Response(
+                response=json.dumps({'msg': 'Authenticated'}),
+                mimetype="application/json",
+                status=200
+            )
+            return authenticate(response, profile)
+        response.status_code = 400
+        return {'error': 'Incorrect username/password combination.'}
+    response.status_code = 400
+    return {'error': 'Some fields are missing'}
+
+
+@app.route('/api/v1/accounts/logout', methods=["GET"])
+def logout():
+    """Log a user out."""
+    return jsonify({'msg': 'Logged out.'})
+
+
+@app.route('/api/v1/accounts/<username>', methods=["GET"])
+@auth.login_required
+def profile_detail(username):
+    """Get the detail for an individual profile."""
+    profile = get_profile(username)
+    if profile:
+        response = Response(
+            mimetype="application/json",
+            response=json.dumps(profile.to_dict()),
+        )
+        return authenticate(response, profile)
+    return notfound_response()
+
+
+@app.route('/api/v1/accounts/<username>/tasks', methods=['GET'])
+@auth.login_required
+def task_list(username):
+    """List all of the tasks for one user."""
+    profile = get_profile(username)
+    if profile:
+        tasks = [task.to_dict() for task in profile.tasks.all()]
+        output = {'username': username, 'tasks': tasks}
+        response = Response(
+            mimetype="application/json",
+            response=json.dumps(output)
+        )
+        return authenticate(response, profile)
+    return notfound_response()
+
+
+@app.route('/api/v1/accounts/<username>/tasks', methods=['POST'])
+@auth.login_required
+def create_task(username):
+    """List all of the tasks for one user."""
+    profile = get_profile(username)
+    if profile:
+        task = Task(
+            name=request.form['name'],
+            note=request.form['note'],
+            creation_date=datetime.now(),
+            due_date=datetime.strptime(due_date, INCOMING_DATE_FMT) if due_date else None,
+            completed=request.form['completed'],
+            profile_id=profile.id,
+        )
+        db.session.add(task)
+        db.session.commit()
+        output = {'msg': 'posted'}
+        response = Response(
+            mimetype="application/json",
+            response=json.dumps(output),
+            status=201
+        )
+        return authenticate(response, profile)
+    return notfound_response()
+
+
+@app.route('/api/v1/accounts/<username>/tasks/<int:task_id>', methods=['GET'])
+@auth.login_required
+def task_detail(username, task_id):
+    """Get the detail for one task if that task belongs to the provided user."""
+    profile = get_profile(username)
+    if profile:
+        task = Task.query.get(task_id)
+        if task in profile.tasks:
+            output = {'username': username, 'task': task.to_dict()}
+            response = Response(
+                mimetype="application/json",
+                response=json.dumps(output)
+            )
+            return authenticate(response, profile)
+    return notfound_response()
+
+
+@app.route('/api/v1/accounts/<username>/tasks/<int:task_id>', methods=['PUT'])
+@auth.login_required
+def task_update(username, task_id):
+    """Update one task if that task belongs to the provided user."""
+    profile = get_profile(username)
+    if profile:
+        task = Task.query.get(task_id)
+        if task in profile.tasks:
+            if 'name' in request.form:
+                task.name = request.form['name']
+            if 'note' in request.form:
+                task.note = request.form['note']
+            if 'completed' in request.form:
+                task.completed = request.form['completed']
+            if 'due_date' in request.form:
+                due_date = request.form['due_date']
+                task.due_date = datetime.strptime(due_date, INCOMING_DATE_FMT) if due_date else None
+            db.session.add(task)
+            db.session.commit()
+            output = {'username': username, 'task': task.to_dict()}
+            response = Response(
+                mimetype="application/json",
+                response=json.dumps(output)
+            )
+            return authenticate(response, profile)
+    return notfound_response()    
+
+
+@app.route('/api/v1/accounts/<username>/tasks/<int:task_id>', methods=['GET'])
+@auth.login_required
+def task_delete(username, task_id):
+    """Delete one task if that task belongs to the provided user."""
+    profile = get_profile(username)
+    if profile:
+        task = Task.query.get(task_id)
+        if task in profile.tasks:
+            db.session.delete(task)
+            db.session.commit()
+            output = {'username': username, 'msg': 'Deleted.'}
+            response = Response(
+                mimetype="application/json",
+                response=json.dumps(output)
+            )
+            return authenticate(response, profile)
+    return notfound_response()    
--- nuertey_flask_project_1/models.py
+++ nuertey_flask_project_1/models.py
@@ -1,7 +1,62 @@
+from .app import db
 import string
 import secrets
-from .app import db
 from datetime import datetime
+
+DATE_FMT = '%d/%m/%Y %H:%M:%S'
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode, nullable=False)
+    note = db.Column(db.Unicode)
+    creation_date = db.Column(db.DateTime, nullable=False)
+    due_date = db.Column(db.DateTime)
+    completed = db.Column(db.Boolean, default=False)
+    profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=False)
+    profile = db.relationship("Profile", back_populates='tasks')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'note': self.note,
+            'creation_date': self.creation_date.strftime(DATE_FMT),
+            'due_date': self.due_date.strftime(DATE_FMT) if self.due_date else None,
+            'completed': self.completed,
+            'profile_id': self.profile_id
+        }
+
+    def __repr__(self):
+        return "<Task: {} | owner: {}>".format(self.name, self.profile.username)
+
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Unicode, nullable=False)
+    email = db.Column(db.Unicode, nullable=False)
+    password = db.Column(db.Unicode, nullable=False)
+    date_joined = db.Column(db.DateTime, nullable=False)
+    token = db.Column(db.Unicode, nullable=False)
+    tasks = db.relationship("Task", back_populates='profile', lazy=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.date_joined = datetime.now()
+        self.token = secrets.token_urlsafe(64)
+
+    def to_dict(self):
+        """Get the object's properties as a dictionary."""
+
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "date_joined": self.date_joined.strftime(DATE_FMT),
+            "tasks": [task.to_dict() for task in self.tasks]
+        }
+
+    def __repr__(self):
+        return "<Profile: {} | tasks: {}>".format(self.username, len(self.tasks))
 
 # Because the model definition occupies a different conceptual space than 
 # the application configuration, make models.py to hold model definitions 
@@ -27,39 +82,39 @@ from datetime import datetime
 # date of creation. You can explicitly set that relationship by effectively 
 # saying, "when an instance of this model is constructed, record the date 
 # and time and set it as the creation date."
-class Task(db.Model):
-    """Tasks for the To Do list."""
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Unicode, nullable=False)
-    note = db.Column(db.Unicode)
-    creation_date = db.Column(db.DateTime, nullable=False)
-    due_date = db.Column(db.DateTime)
-    completed = db.Column(db.Boolean, default=False)
-
-    def __init__(self, *args, **kwargs):
-        """On construction, set date of creation."""
-        super().__init__(*args, **kwargs)
-        self.creation_date = datetime.now()
+#class Task(db.Model):
+#    """Tasks for the To Do list."""
+#    id = db.Column(db.Integer, primary_key=True)
+#    name = db.Column(db.Unicode, nullable=False)
+#    note = db.Column(db.Unicode)
+#    creation_date = db.Column(db.DateTime, nullable=False)
+#    due_date = db.Column(db.DateTime)
+#    completed = db.Column(db.Boolean, default=False)
+#
+#    def __init__(self, *args, **kwargs):
+#        """On construction, set date of creation."""
+#        super().__init__(*args, **kwargs)
+#        self.creation_date = datetime.now()
 
 # Similarly, build the User object:
-class User(db.Model):
-    """The User object that owns tasks."""
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Unicode, nullable=False)
-    email = db.Column(db.Unicode, nullable=False)
-    password = db.Column(db.Unicode, nullable=False)
-    date_joined = db.Column(db.DateTime, nullable=False)
-    token = db.Column(db.Unicode, nullable=False)
-
-    def __init__(self, *args, **kwargs):
-        """On construction, set date of creation."""
-        super().__init__(*args, **kwargs)
-        self.date_joined = datetime.now()
-        # Return a random URL-safe text string, containing nbytes random
-        # bytes. The text is Base64 encoded, so on average each byte results
-        # in approximately 1.3 characters. If nbytes is None or not supplied, 
-        # a reasonable default is used.
-        self.token = secrets.token_urlsafe(64)
+#class User(db.Model):
+#    """The User object that owns tasks."""
+#    id = db.Column(db.Integer, primary_key=True)
+#    username = db.Column(db.Unicode, nullable=False)
+#    email = db.Column(db.Unicode, nullable=False)
+#    password = db.Column(db.Unicode, nullable=False)
+#    date_joined = db.Column(db.DateTime, nullable=False)
+#    token = db.Column(db.Unicode, nullable=False)
+#
+#    def __init__(self, *args, **kwargs):
+#        """On construction, set date of creation."""
+#        super().__init__(*args, **kwargs)
+#        self.date_joined = datetime.now()
+#        # Return a random URL-safe text string, containing nbytes random
+#        # bytes. The text is Base64 encoded, so on average each byte results
+#        # in approximately 1.3 characters. If nbytes is None or not supplied, 
+#        # a reasonable default is used.
+#        self.token = secrets.token_urlsafe(64)
 
 # In a given web application, relationships between objects can be  
 # expressed thus. In the To-Do List, for example, users own multiple 
@@ -76,11 +131,11 @@ class User(db.Model):
 # object with that ID. Also make sure to include a keyword argument
 # (back_populates) that updates the User model when the task gets a user
 # as an owner.
-user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-user = db.relationship("user", back_populates="tasks")
+#user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#user = db.relationship("user", back_populates="tasks")
 
 # For the "one," set a field for the tasks the specific user owns. Similar
 # to maintaining the two-way relationship on the Task object, set a keyword
 # argument on the User's relationship field to update the Task when it is 
 # assigned to a user.
-tasks = db.relationship("Task", back_populates="user")
+#tasks = db.relationship("Task", back_populates="user")
