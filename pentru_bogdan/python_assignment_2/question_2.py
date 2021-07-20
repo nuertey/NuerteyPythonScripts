a5a100f35c987a7c90362ff13d8435854c1ed9ac
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2021-07-20 11:00:13 -0500

    Checkpoint kmeans

--- pentru_bogdan/python_assignment_2/question_2.py
+++ pentru_bogdan/python_assignment_2/question_2.py
@@ -23,4 +23,58 @@ reduced = np.load('delta_reduced.npy')
 print(reduced)
 print()
 
+# Write a function named cluster() that fits a k-means clustering algorithm,
+# and returns a tuple (sklearn.cluster.k_means_.KMeans, np.array). The second
+# element of the tuple is a 1-d array that contains the predictions of k-means
+# clustering, i.e. which cluster each data point belongs to. Please remember
+# how we were generating and using the labels for seeds, movements, iris etc.
+#
+# Use default values for all parameters in KMeans() execept for n_clusters 
+# and random_state.
+def cluster(array, random_state, n_clusters=4):
+    '''
+    Fits and predicts k-means clustering on "array"
+    
+    Parameters
+    ----------
+    array: A numpy array
+    random_state: Random seed, e.g. check_random_state(0)
+    n_clusters: The number of clusters. Default: 4
+    
+    Returns
+    -------
+    A tuple (sklearn.KMeans, np.ndarray)
+    '''
+    
+    model = KMeans(n_clusters=n_clusters, random_state=random_state).fit(array)
+    clusters = model.predict(array)
+    
+    print(model)
+    print()
+    
+    print(clusters)
+    print()
+    
+    return model, clusters
 
+#here we return fitted (model) and predicted (clusters) arrays as a tuple
+
+k_means_t, cluster_t = cluster(reduced, random_state=check_random_state(1), n_clusters=5)
+
+assert_is_instance(k_means_t, sklearn.cluster._kmeans.KMeans)
+assert_is_instance(cluster_t, np.ndarray)
+assert_equal(k_means_t.n_init, 10)
+assert_equal(k_means_t.n_clusters, 5)
+assert_equal(len(cluster_t), len(reduced))
+assert_true((cluster_t < 5).all()) # n_cluster = 5 so labels should be between 0 and 5
+assert_true((cluster_t >= 0).all())
+labels_gold = -1. * np.ones(len(reduced), dtype=np.int)
+mindist = np.empty(len(reduced))
+mindist.fill(np.infty)
+for i in range(5):
+    dist = np.sum((reduced - k_means_t.cluster_centers_[i])**2., axis=1)
+    labels_gold[dist < mindist] = i
+    mindist = np.minimum(dist, mindist)
+assert_true((mindist >= 0.0).all())
+assert_true((labels_gold != -1).all())
+assert_array_equal(labels_gold, cluster_t)
