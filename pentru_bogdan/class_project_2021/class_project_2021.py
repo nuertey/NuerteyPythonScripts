b00983f41af4f5a82df2b268af3aed03ec1acd22
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2021-08-09 13:43:02 -0500

    Checkpoint for vertical data....

--- pentru_bogdan/class_project_2021/class_project_2021.py
+++ pentru_bogdan/class_project_2021/class_project_2021.py
@@ -12,18 +12,38 @@
 #**********************************************************************/
 #!/usr/bin/env python
 
-import math
-import numpy as np
-from numpy import inf
+import re
 import pandas as pd
 
 pd.set_option('display.max_rows', 100)
 pd.set_option('display.min_rows', 100)
 pd.options.mode.chained_assignment = None
 
-# For logarithm divide-by-zero warnings
-np.seterr(divide = 'ignore') 
-#np.seterr(divide = 'warn') 
+def strip_text_from_numbers(laptop_resolution_input):
+    match = re.match(r"([a-z]+)([0-9]+)", laptop_resolution_input, re.I)
+    if match:
+        items = match.groups()
+        print("items:")
+        print(items)
+        print()
+#    else:
+#        print("Caught an unexpected exception:")
+#        print()
+
+    head = laptop_resolution_input.rstrip('0123456789')
+    tail = laptop_resolution_input[len(head):]
+
+    #print("head:")    
+    #print(head)
+    #print()
+
+#    print("tail:") 
+#    print(tail)
+#    print()
+    
+    return tail
+
+    #return items
 
 laptops_data_df = pd.read_csv('laptops.csv')
 
@@ -37,4 +57,43 @@ print()
 
 print('laptops_data_df[\'ScreenResolution\']:')
 print(laptops_data_df['ScreenResolution'])
+print()
+
+# Split on your separator at most once, and take the first piece:
+
+vertical_separator = 'x'
+
+# laptops_resolution_data_df['HorizontalPixelCounts']
+
+# laptops_resolution_data_df['VerticalPixelCounts'] = laptops_data_df['ScreenResolution'].partition(vertical_separator)
+
+#laptops_resolution_data_df['EncodedPixelCounts']
+
+# print('laptops_resolution_data_df[\'HorizontalPixelCounts\']:')
+# print(laptops_resolution_data_df['HorizontalPixelCounts'])
+# print()
+
+#print('laptops_resolution_data_df[\'VerticalPixelCounts\']:')
+#print(laptops_resolution_data_df['VerticalPixelCounts'])
+#print()
+
+#print('laptops_resolution_data_df[\'EncodedPixelCounts\']:')
+#print(laptops_resolution_data_df['EncodedPixelCounts'])
+#print()
+
+#match = re.match(r"([a-z]+)([0-9]+)", laptops_data_df['ScreenResolution'], re.I)
+#if match:
+#    items = match.groups()
+#print(items)
+#print()
+
+#laptops_resolution_data_df = []
+
+# Example of a list comprehension in Python which gives faster processing:
+#laptops_resolution_data_df['VerticalPixelCounts'] = [strip_text_from_numbers(laptop_resolution_string) for laptop_resolution_string in laptops_data_df['ScreenResolution']]
+#
+laptops_resolution_vertical_data = [strip_text_from_numbers(laptop_resolution_string) for laptop_resolution_string in laptops_data_df['ScreenResolution']]
+
+print('laptops_resolution_vertical_data:')
+print(laptops_resolution_vertical_data)
 print()
