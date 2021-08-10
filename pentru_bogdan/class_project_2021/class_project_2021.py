af326f203feadb4f31b1cf7ec1ffbc167f505e7e
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2021-08-10 15:29:24 -0500

    We have encoded data now. See output_3\txt

--- pentru_bogdan/class_project_2021/class_project_2021.py
+++ pentru_bogdan/class_project_2021/class_project_2021.py
@@ -21,17 +21,20 @@ pd.options.mode.chained_assignment = None
 
 laptops_resolution_horizontal_data = []
 laptops_resolution_vertical_data = []
+laptops_resolution_encoded_data = []
 
 def strip_text_from_numbers(laptop_resolution_input):
     match = re.match(r"([a-zA-Z\s]*)([0-9]+)[^r]([0-9]+)", laptop_resolution_input, re.I)
     if match:
         items = match.groups()
-        print("items:")
-        print(items)
-        print()
+        # Uncomment to view output for debug and further understanding:
+        #print("items:")
+        #print(items)
+        #print()
         
         horizontal_pixel_counts = items[-2]
         vertical_pixel_counts = items[-1]
+        encoded_pixel_counts = int(str(horizontal_pixel_counts) + str(vertical_pixel_counts))
     
         print("horizontal_pixel_counts:")    
         print(horizontal_pixel_counts)
@@ -40,9 +43,14 @@ def strip_text_from_numbers(laptop_resolution_input):
         print("vertical_pixel_counts:")    
         print(vertical_pixel_counts)
         print()
-        
+
+        print("encoded_pixel_counts:")    
+        print(encoded_pixel_counts)
+        print()
+                
         laptops_resolution_horizontal_data.append(horizontal_pixel_counts)
         laptops_resolution_vertical_data.append(vertical_pixel_counts)
+        laptops_resolution_encoded_data.append(encoded_pixel_counts)
         # End of function. Void return.
     
 laptops_data_df = pd.read_csv('laptops.csv')
@@ -77,10 +85,10 @@ print()
 #print(laptops_resolution_data_df['EncodedPixelCounts'])
 #print()
 
-# Example of a list comprehension in Python which gives faster processing:
 #laptops_resolution_data_df['VerticalPixelCounts'] = [strip_text_from_numbers(laptop_resolution_string) for laptop_resolution_string in laptops_data_df['ScreenResolution']]
 #
 
+# Example of a list comprehension in Python which gives faster processing:
 [strip_text_from_numbers(laptop_resolution_string) for laptop_resolution_string in laptops_data_df['ScreenResolution']]
 
 print('laptops_resolution_horizontal_data:')
@@ -89,4 +97,8 @@ print()
 
 print('laptops_resolution_vertical_data:')
 print(laptops_resolution_vertical_data)
+print()
+
+print('laptops_resolution_encoded_data:')
+print(laptops_resolution_encoded_data)
 print()
