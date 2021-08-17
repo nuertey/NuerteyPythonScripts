e026d9c939cc7d90ac0141f613ddc1bc459dafb8
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2021-08-16 21:37:40 -0500

    My output so far...

--- pentru_bogdan/Assignment_4/assignment_4.py
+++ pentru_bogdan/Assignment_4/assignment_4.py
@@ -39,6 +39,10 @@ print('Complex Networks Python Package Version:')
 print(nx.__version__)
 print()
 
+# Nuertey comment: Let us bypass the Python FutureWarnings that are 
+# clouding the output by simply outputting several spaces before.
+print('\n\n')
+
 # converting sched_dep_time to 'std' - Scheduled time of departure
 airlines_graph_data['std'] = airlines_graph_data.sched_dep_time.astype(str).str.replace('(\d{2}$)', '') + ':' + airlines_graph_data.sched_dep_time.astype(str).str.extract('(\d{2}$)', expand=False) + ':00'
 
@@ -47,4 +51,21 @@ airlines_graph_data['sta'] = airlines_graph_data.sched_arr_time.astype(str).str.
 
 # converting dep_time to 'atd' - Actual time of departure
 airlines_graph_data['atd'] = airlines_graph_data.dep_time.fillna(0).astype(np.int64).astype(str).str.replace('(\d{2}$)', '') + ':' + airlines_graph_data.dep_time.fillna(0).astype(np.int64).astype(str).str.extract('(\d{2}$)', expand=False) + ':00'
+
+airlines_graph_data['ata'] = airlines_graph_data.arr_time.fillna(0).astype(np.int64).astype(str).str.replace('(\d{2}$)', '') + ':' + airlines_graph_data.arr_time.fillna(0).astype(np.int64).astype(str).str.extract('(\d{2}$)', expand=False) + ':00'
+
+airlines_graph_data['date'] = pd.to_datetime(airlines_graph_data[['year', 'month', 'day']])
+
+# finally we drop the columns we don't need
+airlines_graph_data = airlines_graph_data.drop(columns = ['year', 'month', 'day'])
+
+edgelist_graph_dataframe = nx.from_pandas_edgelist(airlines_graph_data, source='origin', target='dest', edge_attr=True,)
+
+# Nuertey comment: Let us bypass the Python FutureWarnings that are 
+# clouding the output by simply outputting several spaces aft.
+print('\n\n')
+
+print('edgelist_graph_dataframe.nodes():')
+print(edgelist_graph_dataframe.nodes())
+print()
 
