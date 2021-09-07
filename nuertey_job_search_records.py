bbf4c1e66f02b33369fafa18177e995a14b84d36
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2021-09-06 23:51:49 -0500

    Enhanced the job search script with further analysis, functionality and plots.

--- nuertey_job_search_records.py
+++ nuertey_job_search_records.py
@@ -13,10 +13,12 @@
 #**********************************************************************/
 #!/usr/bin/env python
 
-import datetime
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
+from nose.tools import assert_equal
 
 pd.set_option('display.max_rows', 100)
 pd.set_option('display.min_rows', 100)
@@ -122,31 +124,125 @@ print(job_search_records_df)
 print()
 
 # Let's visualize with plotly:
-#figure_1 = px.scatter(job_search_records_df, 
-#                      y="CompanyName", 
-#                      x="DateApplied", 
-#                      title="Nuertey Odzeyem's Historical Job Search Data From illinoisjoblink.illinois.gov",
-#                      color="CompanyName", 
-#                      symbol="CompanyName",
-#                      hover_name="CompanyName",        # Display this column in bold as the tooltip title.
-#                      hover_data={'DateApplied':True,  # Add this column to hover tooltip with default formatting.
-#                                  'CompanyName':False, # Remove this column from hover tooltip.
-#                                  'JobTitle':True      # Add this column to hover tooltip with default formatting.
-#                                 }
-#                      )
-#figure_1.update_traces(marker_size=10)
-#figure_1.show()
+figure_1 = px.scatter(job_search_records_df, 
+                      y="CompanyName", 
+                      x="DateApplied", 
+                      title="Nuertey Odzeyem's Historical Job Search Details - Obtained Via illinoisjoblink.illinois.gov",
+                      color="CompanyName", 
+                      symbol="CompanyName",
+                      hover_name="CompanyName",        # Display this column in bold as the tooltip title.
+                      hover_data={'DateApplied':True,  # Add this column to hover tooltip with default formatting.
+                                  'CompanyName':False, # Remove this column from hover tooltip.
+                                  'JobTitle':True      # Add this column to hover tooltip with default formatting.
+                                 }
+                      )
+figure_1.update_traces(marker_size=10)
+figure_1.show()
 
+# ======================================================================
 # Further analysis ensues:
+# ======================================================================
 monthly_analysis_df = job_search_records_df.copy()
 monthly_analysis_df['MonthOfYear'] = monthly_analysis_df['DateApplied'].dt.strftime('%B, %Y')
+
+# Deleting columns without having to reassign the DataFrame:
+monthly_analysis_df.drop('CompanyName', axis=1, inplace=True)
+monthly_analysis_df.drop('JobTitle', axis=1, inplace=True)
 
 print('monthly_analysis_df:')
 print(monthly_analysis_df)
 print()
 
-month_of_year_list = list(range())
+frequency_analysis_df = monthly_analysis_df.groupby('MonthOfYear').count()
+frequency_analysis_df.rename(columns={'DateApplied':'FrequencyCount'}, inplace=True)
 
-baseline_date = datetime.datetime.today()
-number_of_days = 100
-month_of_year_list = [baseline_date - datetime.timedelta(days=x) for x in range(numdays)]
+print('frequency_analysis_df:')
+print(frequency_analysis_df)
+print()
+
+MonthOfYear_list = frequency_analysis_df.index.values.tolist()
+MonthOfYear_dates_list = [datetime.strptime(some_date, '%B, %Y').date() for some_date in MonthOfYear_list]
+MonthOfYear_dates_list = sorted(MonthOfYear_dates_list)
+
+#print('MonthOfYear_dates_list:')
+#print(MonthOfYear_dates_list)
+#print()
+#
+#print('len(MonthOfYear_dates_list):')
+#print(len(MonthOfYear_dates_list))
+#print()
+
+FrequencyCount_list = frequency_analysis_df.FrequencyCount.values.tolist()
+
+#print('FrequencyCount_list:')
+#print(FrequencyCount_list)
+#print()
+#
+#print('len(FrequencyCount_list):')
+#print(len(FrequencyCount_list))
+#print()
+
+assert_equal(len(FrequencyCount_list), len(MonthOfYear_dates_list))
+
+month_of_year_datapoints = []
+
+start_date = '2021-10-1'
+end_date = '2015-6-1'
+
+current_datapoint = begin_datapoint = datetime.strptime(start_date, '%Y-%m-%d').date()
+end_datapoint = datetime.strptime(end_date, '%Y-%m-%d').date()
+
+while current_datapoint >= end_datapoint:
+    month_of_year_datapoints.append(current_datapoint)
+    current_datapoint -= relativedelta(months=1)
+
+#print('month_of_year_datapoints:')
+#print(month_of_year_datapoints)
+#print()
+#
+#print('len(month_of_year_datapoints):')
+#print(len(month_of_year_datapoints))
+#print()
+
+frequency_of_job_applications_datapoints = []
+frequency_of_job_applications_datapoints = [0 for x in range(0, len(month_of_year_datapoints))]
+
+assert_equal(len(frequency_of_job_applications_datapoints), len(month_of_year_datapoints))
+
+# Note that zip runs only up to the shorter of the two lists(not a problem
+# for equal length lists), but, in case of unequal length lists if you want
+# to traverse the whole list then use itertools.izip_longest.
+for index, (month, count) in enumerate(zip(month_of_year_datapoints, frequency_of_job_applications_datapoints)):
+    for value1, value2 in zip(MonthOfYear_dates_list, FrequencyCount_list):
+        if month == value1:
+            # It is safe to do this as we only modify elements we have already traversed.
+            frequency_of_job_applications_datapoints[index] = value2
+            break
+
+#print('frequency_of_job_applications_datapoints:')
+#print(frequency_of_job_applications_datapoints)
+#print()
+#
+#print('len(frequency_of_job_applications_datapoints):')
+#print(len(frequency_of_job_applications_datapoints))
+#print()
+
+# Create a DataFrame for plotting:
+frequency_count_data_df = pd.DataFrame(
+    {'MonthOfYear': month_of_year_datapoints,
+     'NumberOfApplications': frequency_of_job_applications_datapoints
+    })
+
+print('frequency_count_data_df:')
+print(frequency_count_data_df)
+print()
+
+figure_2 = px.bar(frequency_count_data_df, 
+                        x="MonthOfYear", 
+                        y="NumberOfApplications", 
+                        title="Nuertey Odzeyem's Historical Job Search Frequency Distribution - Obtained Via illinoisjoblink.illinois.gov",
+                        hover_data=['MonthOfYear', 'NumberOfApplications'],
+                        color='NumberOfApplications',
+                        labels={'MonthOfYear':'Year and Month of Job Application'}
+                        )
+figure_2.show()
