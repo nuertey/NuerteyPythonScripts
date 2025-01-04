7c31e0d03128db01226823db62d351834aa329c6
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2025-01-04 00:26:13 -0600

    Enhanced example 5 for actual asyncio mqtt on test.mosquitto.org.

--- asyncio_assays/script_5.py
+++ asyncio_assays/script_5.py
@@ -7,17 +7,45 @@
 #     Compatible with async code
 #     Fully type-hinted
 #     Did we mention no more callbacks?
+import threading
+import asyncio
+from asyncio_mqtt import Client
 
-Subscriber
+async def subscriber_coroutine():
+    async with Client("test.mosquitto.org") as client:
+        topic = "humidity/#"
+        async with client.filtered_messages(topic) as messages:
+            print(f"Subscribing to topic: {topic} ...")
+            await client.subscribe(topic)
+            async for message in messages:
+                try:
+                    print(f"Received message: {message.payload.decode()}")
+                except:
+                    print(f"Received message: {message.payload}")
 
-async with Client("test.mosquitto.org") as client:
-    async with client.messages() as messages:
-        await client.subscribe("humidity/#")
-        async for message in messages:
-            print(message.payload)
+async def publisher_coroutine():
+    async with Client("test.mosquitto.org") as client:
+        topic = "humidity/outside"
+        payload = 0.38
+        print(f"Publishing ...\n\ttopic = {topic}\n\tpayload = {payload}")
+        await client.publish(topic, payload=payload)
 
-Publisher
+def subscriber():
+    asyncio.run(subscriber_coroutine())
 
-async with Client("test.mosquitto.org") as client:
-    await client.publish("humidity/outside", payload=0.38)
+def publisher():
+    asyncio.run(publisher_coroutine())
+
+if __name__ ==  '__main__':
+    thread_1 = threading.Thread(target=subscriber, name="MQTT_Subscriber")
+    thread_1.daemon = True 
+
+    thread_2 = threading.Thread(target=publisher, name="MQTT_Publisher")
+    thread_2.daemon = True 
+    
+    thread_1.start()
+    thread_2.start()
+    
+    thread_2.join()
+    thread_1.join()
 
