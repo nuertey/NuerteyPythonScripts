55ea6d58a5d9d7eef73709949b5a56c6ca7be121
Nuertey Odzeyem <nuertey_odzeyem@hotmail.com>
2025-01-05 09:58:19 -0600

    Added aiohttp server example code. Execution output is as follows:
    python script_7.py
    
    ======== Running on http://0.0.0.0:8080 ========
    (Press CTRL+C to quit)

---
+++ asyncio_assays/script_7.py
@@ -0,0 +1,16 @@
+# Server example:
+
+from aiohttp import web
+
+async def handle(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = "Hello, " + name
+    return web.Response(text=text)
+
+app = web.Application()
+app.add_routes([web.get('/', handle),
+                web.get('/{name}', handle)])
+
+if __name__ == '__main__':
+    web.run_app(app)
+
